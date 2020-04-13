86898244db49b2a91eb3f64a28a4b1868b73a7a5
Pavel_M <dobrko89@gmail.com>
2020-04-13 19:23:27 +0300

    Add loop

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-'use strict';
+"use strict";
 
+let money = +prompt("Ваш денехный лимит на месяц?");
+let time = prompt("Enter data in YYYY-MM-DD format");
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
 
-var LeftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+  let b = prompt("Во сколько обойдется?", "");
 
-console.log(LeftBorderWidth);
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    appData.expenses[a] = b;
+  } else {
+    console.log("Плохой результат");
+    i--;
+  }
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Произошла ошибка");
+}
